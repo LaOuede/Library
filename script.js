d9bf00ff2947ea43f9c1d06604ce286d2cd668b0
Gwenola Le Roux <gwenola.leroux@gmail.com>
2024-07-04 10:59:11 -0400

    Library: refactor with classes

--- script.js
+++ script.js
@@ -1,131 +1,140 @@
-const myLibrary = [];
-
-const addBtn = document.querySelector('.addBtn');
-const myBooks = document.querySelector('.myBooks');
-const confirmBtn = document.querySelector('.confirmBtn');
-const cancelBtn = document.querySelector('.cancelBtn');
-const form = document.querySelector('#newBook');
-const dialog = document.querySelector('#dialog');
-const output = document.querySelector('output');
-const btnDel = document.querySelector('.btnDel');
-
-function Book(title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
-
-	this.info = function() {
-		let infos = `<b>${title}</b><br>by<br><b>${author}</b><br><br>${pages} pages<br><br>`;
+// Book Class
+class Book {
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
+	
+	info() {
+		let infos =`<b>${this.title}</b><br>by<br><b>${this.author}</b><br><br>${this.pages} pages<br><br>`;
 		return infos;
 	}
-
-	this.isRead = function() {
+	
+	isRead() {
 		this.read = !this.read;
 	}
-};
-
-function addBookToLibrary(title, author, pages, read) {
-	const newBook = new Book(title, author, pages, read);
-	myLibrary.push(newBook);
-	renderLibrary();
 }
 
+// Library Class
+class Library {
+	constructor() {
+		this.myLibrary = [];
+		this.addBtn = document.querySelector('.addBtn');
+		this.myBooks = document.querySelector('.myBooks');
+		this.confirmBtn = document.querySelector('.confirmBtn');
+		this.cancelBtn = document.querySelector('.cancelBtn');
+		this.form = document.querySelector('#newBook');
+		this.dialog = document.querySelector('#dialog');
+		this.output = document.querySelector('output');
+		this.btnDel = document.querySelector('.btnDel');
+		this.handleEventsListeners();
+	}
 
-function renderLibrary() {
-	myBooks.innerHTML = '';
-	
-	if (myLibrary.length === 0) {
-		const noBook = document.createElement('li');
-		noBook.textContent = "No book in the library yet...";
-		myBooks.appendChild(noBook);
-	} else {
-		myLibrary.forEach((book, index) => {
-			const newBook = document.createElement('div');
-			newBook.className = 'book';
-			const btnDel = createButton('Delete', 'btnDel', index);
-			const isRead = (myLibrary[index].read) ? 'Read' : 'Not read';
-			const btnRead = createButton(isRead, 'btnRead', index);
+	handleEventsListeners() {
+		document.addEventListener('keydown', (event) => {
+			if (event.key === 'Escape') {
+				this.dialog.close();
+				document.body.classList.remove('dialog-open');
+			}
+		})
+
+		this.addBtn.addEventListener('click', () => {
+			this.dialog.showModal();
+			document.body.classList.add('dialog-open');
+		})
+		
+		this.cancelBtn.addEventListener('click', () => {
+			dialog.close();
+			document.body.classList.remove('dialog-open');
+		})
+
+		this.form.addEventListener('submit', (event) => {
+			event.preventDefault();
 			
-			newBook.innerHTML = book.info();
-			newBook.appendChild(btnRead);
-			newBook.appendChild(btnDel);
-			myBooks.appendChild(newBook);
-		});
+			const title = document.querySelector('#title').value;
+			const author = document.querySelector('#author').value;
+			let pages = document.querySelector('#pages').value || '-';
+			const read = document.querySelector('#read').checked;
+			
+			const newBook = new Book(title, author, pages, read);
+			this.myLibrary.push(newBook);
+			console.log(this.myLibrary);
+			this.renderLibrary();
+			this.dialog.close();
+			this.form.reset();
+			document.body.classList.remove('dialog-open');
+		})
+		
+		document.addEventListener('click', (event) => {
+			const index = event.target.getAttribute('data-index');
+			if (event.target && event.target.id === 'btnDel') {
+				this.myLibrary.splice(index, 1);
+				this.renderLibrary();
+			} else if (event.target && event.target.id === 'btnRead') {
+				this.myLibrary[index].isRead();
+				this.renderLibrary();
+			};
+		})
 	}
-}
 
-const createButton = (text, id, index) => {
-	const button = document.createElement('button');
-	button.textContent = text;
-	button.setAttribute('data-index', index);
-	button.id = id;
-
-	if (id === 'btnRead') {
-		((myLibrary[index].read)
-			? button.style.borderColor = '#00FF0080'
-			: button.style.borderColor = '#FF000080');
+	addBookToLibrary(title, author, pages, read) {
+		const newBook = new Book(title, author, pages, read);
+		this.myLibrary.push(newBook);
+		this.renderLibrary();
 	}
-	return button;
-}
 
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
-
-	const title = document.querySelector('#title').value;
-	const author = document.querySelector('#author').value;
-	let pages = document.querySelector('#pages').value;
-	const read = document.querySelector('#read').checked;
-
-	pages = (!pages) ? '-' : pages;
-	addBookToLibrary(title, author, pages, read);
+	renderLibrary() {
+		this.myBooks.innerHTML = '';
 	
-	console.log(myLibrary);
-	dialog.close();
-	form.reset();
-	document.body.classList.remove('dialog-open');
-});
-
-addBtn.addEventListener('click', () => {
-	dialog.showModal();
-	document.body.classList.add('dialog-open');
-})
-
-cancelBtn.addEventListener('click', (event) => {
-	dialog.close();
-	document.body.classList.remove('dialog-open');
-});
-
-document.addEventListener('keydown', (event) => {
-	if (event.key === 'Escape') {
-		dialog.close();
-		document.body.classList.remove('dialog-open');
+		if (this.myLibrary.length === 0) {
+			const noBook = document.createElement('li');
+			noBook.textContent = "No book in the library yet...";
+			this.myBooks.appendChild(noBook);
+		} else {
+			this.myLibrary.forEach((book, index) => {
+				const newBook = document.createElement('div');
+				newBook.className = 'book';
+				const btnDel = this.createButton('Delete', 'btnDel', index);
+				const isRead = (this.myLibrary[index].read) ? 'Read' : 'Not read';
+				const btnRead = this.createButton(isRead, 'btnRead', index);
+				
+				newBook.innerHTML = book.info();
+				newBook.appendChild(btnRead);
+				newBook.appendChild(btnDel);
+				this.myBooks.appendChild(newBook);
+			});
+		}
 	}
-});
 
-document.addEventListener('DOMContentLoaded', () => {
-	document.addEventListener('click', (event) => {
-		const index = event.target.getAttribute('data-index');
-		if (event.target && event.target.id === 'btnDel') {
-			myLibrary.splice(index, 1);
-			renderLibrary();
-		} else if (event.target && event.target.id === 'btnRead') {
-			myLibrary[index].isRead();
-			renderLibrary();
-		};
-	});
-});
-
-function mokeData() {
-	addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
-	addBookToLibrary('Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', 309, true);
-	addBookToLibrary('A Game of Thrones', 'George R.R. Martin', 694, true);
-	addBookToLibrary('The Lies of Locke Lamora', 'Scott Lynch', 722, false);
-	addBookToLibrary('The Way of Kings', 'Brandon Sanderson', 1007, false);
-	addBookToLibrary('The Eye of the World', 'Robert Jordan', 784, false);
-	addBookToLibrary('The Chronicles of Narnia: The Lion, the Witch and the Wardrobe', 'C.S. Lewis', 206, false);
-	console.log(myLibrary);
-	renderLibrary();
+	createButton(text, id, index) {
+		const button = document.createElement('button');
+		button.textContent = text;
+		button.setAttribute('data-index', index);
+		button.id = id;
+	
+		if (id === 'btnRead') {
+			((this.myLibrary[index].read)
+				? button.style.borderColor = '#00FF0080'
+				: button.style.borderColor = '#FF000080');
+		}
+		return button;
+	}
 }
 
-mokeData();
+// Moke data
+document.addEventListener('DOMContentLoaded', () => {
+	const library = new Library();
+
+	library.addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+	library.addBookToLibrary('Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', 309, true);
+	library.addBookToLibrary('A Game of Thrones', 'George R.R. Martin', 694, true);
+	library.addBookToLibrary('The Lies of Locke Lamora', 'Scott Lynch', 722, false);
+	library.addBookToLibrary('The Way of Kings', 'Brandon Sanderson', 1007, false);
+	library.addBookToLibrary('The Eye of the World', 'Robert Jordan', 784, false);
+	library.addBookToLibrary('The Chronicles of Narnia: The Lion, the Witch and the Wardrobe', 'C.S. Lewis', 206, false);
+	console.log(library.myLibrary);
+
+	library.renderLibrary();
+});
